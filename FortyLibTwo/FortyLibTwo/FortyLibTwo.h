4ecfb7eb3921660710fe48731421827829c9f1d1
Glauco Beserra <gbeserra@uolinc.com>
2021-11-01 18:47:02 -0300

    config

---
+++ FortyLibTwo/FortyLibTwo/FortyLibTwo.h
@@ -0,0 +1,18 @@
+//
+//  FortyLibTwo.h
+//  FortyLibTwo
+//
+//  Created by Glauco Dantas Beserra on 01/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FortyLibTwo.
+FOUNDATION_EXPORT double FortyLibTwoVersionNumber;
+
+//! Project version string for FortyLibTwo.
+FOUNDATION_EXPORT const unsigned char FortyLibTwoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FortyLibTwo/PublicHeader.h>
+
+
